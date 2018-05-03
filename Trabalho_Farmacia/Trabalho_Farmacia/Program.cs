6d5549601b33f3d558605ae5fde53d8fc04f6552
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-05-03 17:02:20 -0300

    Classes criada

---
+++ Trabalho_Farmacia/Trabalho_Farmacia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Farmacia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new SistemaClientes();
+            new SistemaProdultos();
+            new SistemaFuncionarios();
+            new SistemaVagas();
+        }
+    }
+}
